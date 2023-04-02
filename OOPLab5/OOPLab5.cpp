f1ef6fbc9e0e4612e113efa52c270d37985576f6
cowboboy <r2d2tv@yandex.ru>
2023-04-02 23:37:42 +0500

    Добавьте файлы проекта.

---
+++ OOPLab5/OOPLab5.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+
+class Transport {
+private:
+    int maxSpeed;
+public:
+    Transport() {
+        printf("Transport()\n");
+    }
+    int getMaxSpeed() {
+        printf("Transport::getMaxSpeed()\n");
+        return maxSpeed;
+    }
+    virtual void drive() = 0;
+    ~Transport() {
+        printf("~Transport()\n");
+    }
+};
+
+class Motocycle : public Transport {
+private:
+    bool sidecarMotocycle;
+public:
+    Motocycle() {
+        printf("Motocycle()\n");
+    }
+    void drive() override {
+        printf("Motocycle::drive() override\n");
+    }
+    ~Motocycle() {
+        printf("~Motocycle()\n");
+    }
+};
+
+class Truck : public Transport {
+private:
+    int loadCapacity;
+public:
+    Truck() {
+        printf("Truck()\n");
+    }
+    void drive() override {
+        printf("Truck::drive() override\n");
+    }
+    ~Truck() {
+        printf("~Truck()\n");
+    }
+};
+
+class ElectricCar : public Transport {
+private:
+    int electricMotorCapacity;
+public:
+    ElectricCar() {
+        printf("ElectricCar()\n");
+    }
+    void drive() override {
+        printf("ElectricCar::drive() override\n");
+    }
+    ~ElectricCar() {
+        printf("~ElectricCar()\n");
+    }
+};
+
+int main()
+{
+    Transport* transport = new Truck();
+    Truck truck;
+    truck.getMaxSpeed();
+    transport->drive();
+}
