d1fbacfbdb15bf5727c7fb91c778f08b0b5257d7
cowboboy <r2d2tv@yandex.ru>
2023-04-03 00:22:20 +0500

    some changes

--- OOPLab5/OOPLab5.cpp
+++ OOPLab5/OOPLab5.cpp
@@ -1,8 +1,31 @@
 ﻿#include <iostream>
 
-class Transport {
+class Motor {
+public:
+    Motor() {
+        printf("Motor()\n");
+    }
+    ~Motor() {
+        printf("~Motor()\n");
+    }
+};
+
+class ElectricMotor : public Motor {
 private:
+    int electricMotorCapacity;
+public:
+    ElectricMotor() {
+        printf("ElectricMotor()\n");
+    }
+    ~ElectricMotor() {
+        printf("~ElectricMotor()\n");
+    }
+};
+
+class Transport {
+protected:
     int maxSpeed;
+    Motor* motor;
 public:
     Transport() {
         printf("Transport()\n");
@@ -11,15 +34,16 @@ public:
         printf("Transport::getMaxSpeed()\n");
         return maxSpeed;
     }
+    virtual bool isInstance(std::string example) {
+        return example == "Transport";
+    }
     virtual void drive() = 0;
-    ~Transport() {
+    virtual ~Transport() {
         printf("~Transport()\n");
     }
 };
 
 class Motocycle : public Transport {
-private:
-    bool sidecarMotocycle;
 public:
     Motocycle() {
         printf("Motocycle()\n");
@@ -27,8 +51,27 @@ public:
     void drive() override {
         printf("Motocycle::drive() override\n");
     }
+    bool isInstance(std::string example) override{
+        return example == "Motorcycle" || Transport::isInstance(example);
+    }
     ~Motocycle() {
         printf("~Motocycle()\n");
+    }
+};
+
+class SidecarMotocycle : public Motocycle {
+public:
+    SidecarMotocycle() {
+        printf("~SidecarMotocycle()\n");
+    }
+    void drive() override {
+        printf("SidecarMotocycle::drive() override\n");
+    }
+    bool isInstance(std::string example) override {
+        return example == "SidecarMotocycle" || Motocycle::isInstance(example);
+    }
+    ~SidecarMotocycle() {
+        printf("~SidecarMotocycle()\n");
     }
 };
 
@@ -42,30 +85,71 @@ public:
     void drive() override {
         printf("Truck::drive() override\n");
     }
+    bool isInstance(std::string example) override {
+        return example == "Truck" || Transport::isInstance(example);
+    }
     ~Truck() {
         printf("~Truck()\n");
     }
 };
 
 class ElectricCar : public Transport {
-private:
-    int electricMotorCapacity;
 public:
     ElectricCar() {
         printf("ElectricCar()\n");
+        motor = new ElectricMotor();
     }
     void drive() override {
         printf("ElectricCar::drive() override\n");
     }
+    bool isInstance(std::string example) override {
+        return example == "ElectricCar" || Transport::isInstance(example);
+    }
     ~ElectricCar() {
         printf("~ElectricCar()\n");
+        delete motor;
     }
 };
 
 int main()
 {
+    srand(time(0));
     Transport* transport = new Truck();
     Truck truck;
-    truck.getMaxSpeed();
-    transport->drive();
+    truck.getMaxSpeed(); // наследуемый метод
+    transport->drive(); // перекрытый метод
+    delete transport;
+    
+    ElectricCar electricCar;
+    transport = new ElectricCar();
+    delete transport; // утечка памяти не происходит, так как конструктор базового класса виртуальный
+
+    Transport** transports = new Transport*[10];
+    for (int i = 0; i < 10; ++i) {
+        int choise = rand() % 3;
+        printf("%d", choise);
+        switch (choise) {
+        case 0:
+            transports[i] = new ElectricCar();
+            break;
+        case 1:
+            transports[i] = new SidecarMotocycle();
+            break;
+        case 2:
+            transports[i] = new Truck();
+            break;
+        }
+    }
+    // проверки на принадлежность некоторому классу
+    for (int i = 0; i < 10; ++i) {
+        if (transports[i]->isInstance("SidecarMotocycle")) {
+            printf("Instance of SidecarMotocycle\n");
+        }
+        else if (transports[i]->isInstance("ElectricCar")) {
+            printf("Instance of ElectricCar\n");
+        }
+        else if (transports[i]->isInstance("Truck")) {
+            printf("Instance of Truck\n");
+        }
+    }
 }
